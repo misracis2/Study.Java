80931f2ea643aa2393b9412bc0a7836afae5cc04
이승열 <116939888+misracis2@users.noreply.github.com>
2022-12-03 11:07:21 +0900

    Add files via upload
    혼공자

---
+++ 혼자공부하는자바/chap08/src/main/java/com/example/chap08/sec01/exam01/RemoteControl.java
@@ -0,0 +1,4 @@
+package com.example.chap08.sec01.exam01;
+
+public interface RemoteControl {
+}
---
+++ 혼자공부하는자바/chap08/src/main/java/com/example/chap08/sec01/exam02/RomoteControl.java
@@ -0,0 +1,7 @@
+package com.example.chap08.sec01.exam02;
+
+public interface RomoteControl {
+    int MAX_VOLUME = 10;
+    int MIN_VOLUME = 0;
+
+}
